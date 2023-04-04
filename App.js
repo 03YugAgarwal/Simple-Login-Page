516760a4bf433d83df573ec177a17f168be9f0ed
Yug <03yugagarwal@gmail.com>
2023-04-04 08:57:51 +0530

    initial commit

--- App.js
+++ App.js
@@ -1,10 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View, Alert } from "react-native";
 
 export default function App() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  handleLoginClick = (e) => {
+    Alert.alert(`Logged in with username ${username} and password ${password}`)
+    setUsername('')
+    setPassword('')
+  };
+  
+  handleRegisterClick = (e) => {
+    Alert.alert(`Registered with username ${username} and password ${password}`)
+    setUsername('')
+    setPassword('')
+    // Alert.alert('Simple Button pressed')
+  };
+
+  handleUserChange = (e) => {
+    setUsername(e);
+  };
+
+  handlePassChange = (e) => {
+    setPassword(e);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.textPrimary}>VIT-Chat</Text>
+      <Text style={styles.textSecondary}>Login</Text>
+      <Text style={styles.text}>Username:</Text>
+      <TextInput
+        value={username}
+        onChangeText={handleUserChange}
+        style={styles.textInput}
+        placeholder="e.g. Yug Agarwal"
+      />
+      <Text style={styles.text}>Password:</Text>
+      <TextInput
+        value={password}
+        onChangeText={handlePassChange}
+        style={styles.textInput}
+        placeholder="e.g. 12345678"
+      />
+      <Button title="Login" style={styles.button} onPress={handleLoginClick}/>
+      <Button title="Register" style={styles.button} onPress={handleRegisterClick}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +55,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#eee",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textPrimary: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  textSecondary: {
+    fontSize: 20,
+    marginBottom: 100,
+  },
+  textInput: {
+    borderColor: "black",
+    padding: 5,
+    borderWidth: 2,
+    width: 200,
+    marginBottom: 20,
+    borderRadius: 7,
   },
 });
